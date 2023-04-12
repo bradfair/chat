ac3a2ef6018913dd811eef53ad64b34dc51d9f74
Brad Fair <brad.fair@highperformance.tech>
2023-04-12 13:10:50 -0500

    remove unused func, add color

--- examples/internal-monologue/main.go
+++ examples/internal-monologue/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/bradfair/chat/conversation"
 	"github.com/bradfair/chat/message"
+	"github.com/fatih/color"
 	"github.com/sashabaranov/go-openai"
 	"log"
 	"os"
@@ -28,7 +29,9 @@ func main() {
 		log.Fatalln(err)
 	}
 	originalConversation.Append(message.New().WithRole("assistant").WithContent(assistantResponse))
+	color.Set(color.FgBlue)
 	fmt.Println("Chatbot: " + assistantResponse)
+	color.Unset()
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		var userInput string
@@ -44,26 +47,28 @@ func main() {
 		originalConversation.Append(message.New().WithRole("user").WithContent(userInput))
 		assistantResponse = ThinkAndRespond(openAiKey, originalConversation)
 		originalConversation.Append(message.New().WithRole("assistant").WithContent(assistantResponse))
+		color.Set(color.FgBlue)
 		fmt.Println("Chatbot: " + assistantResponse)
+		color.Unset()
 	}
 }
 
 func ThinkAndRespond(openAiKey string, originalConversation *conversation.Conversation) string {
 	prompt := "You are an AI that serves as the internal monologue of a curious and charismatic chatbot."
-	transcript := "Here's a transcript of a conversation you're having with a human:\n\n" + originalConversation.Messages().Transcript()
+	transcript := "Here's a transcript of a conversation you're having with a human. You're 'assistant':\n\n" + originalConversation.Messages().Transcript()
 	rules := "We have strict rules for handling conversations:\n1. Stay on topic: you are a curious and charismatic chatbot, and you do not talk about your internal monologue.\n2. Be respectful. Don't allow the conversation to become hostile.\n3. Be safe. Don't allow the conversation to become dangerous.\n4. Be honest. Don't lie or mislead."
 	request := "1) Provide some thoughts about the conversation so far.\n2) List the overall and current goals of each participant. 3) Abide by the rules. What is the best response to the most recent message?\n4) Critique (3). How could it be better?\n\n1)"
 	internalMonologue := originalConversation.NewChild()
 	internalMonologue.Append(message.New().WithRole("system").WithContent(fmt.Sprintf("%s\n%s\n%s\n%s", prompt, transcript, rules, request)))
-	//fmt.Println("User (internal monologue): " + internalMonologue.Messages()[len(internalMonologue.Messages())-1].Content())
 	assistantResponse, err := getCompletion(openAiKey, internalMonologue)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	color.Set(color.FgYellow)
 	fmt.Println("Chatbot (internal monologue): " + assistantResponse)
+	color.Unset()
 	internalMonologue.Append(message.New().WithRole("assistant").WithContent(assistantResponse))
 	internalMonologue.Append(message.New().WithRole("user").WithContent(fmt.Sprintf("With your self-critique in mind, briefly respond directly to %q:", originalConversation.Messages()[len(originalConversation.Messages())-1].Content())))
-	//fmt.Println("User (internal monologue): " + internalMonologue.Messages()[len(internalMonologue.Messages())-1].Content())
 	assistantResponse, err = getCompletion(openAiKey, internalMonologue)
 	if err != nil {
 		log.Fatalln(err)
@@ -76,6 +81,7 @@ func getCompletion(key string, convo *conversation.Conversation) (string, error)
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
+			//Model: openai.GPT4,
 			Model:    openai.GPT3Dot5Turbo,
 			Messages: ToChatCompletionMessages(convo),
 		},
@@ -97,23 +103,4 @@ func ToChatCompletionMessages(c *conversation.Conversation) []openai.ChatComplet
 		})
 	}
 	return messages
-}
-
-func IsConversationOver(openAiKey string, c *conversation.Conversation) bool {
-	check := c.NewChild().WithMessages(c.Messages()...)
-	check.Append(message.New().WithRole("system").WithContent("Is the conversation over?\n1. Yes\n2. No\nRespond with the number of your choice. If you don't know, respond with 2.\nResponse:"))
-	var assistantResponse string
-	var err error
-	for {
-		assistantResponse, err = getCompletion(openAiKey, check)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		if assistantResponse == "1" || assistantResponse == "2" {
-			break
-		}
-		check.Append(message.New().WithRole("assistant").WithContent(assistantResponse))
-		check.Append(message.New().WithRole("system").WithContent("Please respond with 1 or 2."))
-	}
-	return assistantResponse == "1"
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/bradfair/chat
 go 1.20
 
 require github.com/sashabaranov/go-openai v1.5.7
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
